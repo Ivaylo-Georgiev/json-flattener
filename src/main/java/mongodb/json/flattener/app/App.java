e0f76ca34b11c1aebb4b610e6ea08ce5dbf39cb4
Ivaylo Georgiev <ivaylo.georgiev18@gmail.com>
2021-03-30 23:28:29 +0300

    run program in a loop until terminated by the user

--- src/main/java/mongodb/json/flattener/app/App.java
+++ src/main/java/mongodb/json/flattener/app/App.java
@@ -1,5 +1,6 @@
 package mongodb.json.flattener.app;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,14 +9,33 @@ import mongodb.json.flattener.command.CommandHandler;
 public class App {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("    _                        __ _       _   _                        \r\n" + 
+				"   (_)___  ___  _ __        / _| | __ _| |_| |_ ___ _ __   ___ _ __  \r\n" + 
+				"   | / __|/ _ \\| '_ \\ _____| |_| |/ _` | __| __/ _ \\ '_ \\ / _ \\ '__| \r\n" + 
+				"   | \\__ \\ (_) | | | |_____|  _| | (_| | |_| ||  __/ | | |  __/ |    \r\n" + 
+				"  _/ |___/\\___/|_| |_|     |_| |_|\\__,_|\\__|\\__\\___|_| |_|\\___|_|    \r\n" + 
+				" |__/                                                                \n");
+		
 		CommandHandler commandHandler = new CommandHandler();
-		System.out.print(">");
-		String command = commandHandler.readCommand(new InputStreamReader(System.in));
-		if (commandHandler.validateCommand(command)) {
-			commandHandler.executeCommand(command);
-		} else {
-			System.out.println(
-					"Invalid command. Your command should match to the following pattern: cat <filename> | flatten");
+
+		try (BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in))) {
+			String command = null;
+			while (true) {
+				System.out.print(">");
+				command = commandHandler.readCommand(commandReader);
+
+				if (commandHandler.isQuitCommand(command)) {
+					System.out.println("Program terminated successfully");
+					break;
+				}
+
+				if (commandHandler.validateCommand(command)) {
+					commandHandler.executeCommand(command);
+				} else {
+					System.out.println(
+							"Invalid command. Your command should match the following pattern: cat <filename> | flatten. To quit, type 'quit'");
+				}
+			}
 		}
 	}
 
